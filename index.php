7471208e498807d482e35b06b57b080d2ffb3731
Thibault Vacheron <thib.du-42@hotmail.fr>
2018-11-30 14:46:26 +0100

    css html et h1

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+
 <?php 
 echo "Hello world!"; 
 ?>
+<!DOCTYPE html>
+<html>
+    <head>
+
+        <title>température</title>
+        <link rel="stylesheet" media="screen" type="text/css" title="style"         href="main.css"/>
+
+    </head>
+    <body>
+        <h1>Température</h1>
+
+
+    </body>
+
+</html>
